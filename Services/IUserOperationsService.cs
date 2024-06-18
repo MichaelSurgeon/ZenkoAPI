3d842d02a1d017ca03feaf46250f10b34e188c20
Michael Surgeon <m.surgeon@craneware.com>
2024-06-18 23:24:14 +0100

    Add services

---
+++ Services/IUserOperationsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using ZenkoAPI.Models;
+
+namespace ZenkoAPI.Services
+{
+    public interface IUserOperationsService
+    {
+        Task CreateUserAsync(User user);
+        Task<User> GetUserBasedOnEmail(User user);
+    }
+}
---
+++ Services/UserOperationsService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZenkoAPI.Data;
+using ZenkoAPI.Models;
+using ZenkoAPI.Repositories;
+
+namespace ZenkoAPI.Services
+{
+    public class UserOperationsService(DatabaseContext databaseContext, IAccountRepository accountRepository) : IUserOperationsService
+    {
+        public async Task CreateUserAsync(User user)
+        {
+            var newUser = new User()
+            {
+                UserId = new Guid(),
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Password = user.Password,
+                Address = user.Address
+            };
+
+            await accountRepository.AddUserToDatabaseAsync(newUser);
+        }
+
+        public async Task<User> GetUserBasedOnEmail(User user) => await databaseContext.Users.FirstOrDefaultAsync(row => row.Email == user.Email);
+    }
+}
