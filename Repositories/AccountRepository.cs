c5f85e86cc1463ea3fb3c5a6675e1b8b64186239
Michael Surgeon <m.surgeon@craneware.com>
2024-06-18 23:23:29 +0100

    ...

---
+++ Repositories/AccountRepository.cs
@@ -0,0 +1,29 @@
+﻿using ZenkoAPI.Data;
+using ZenkoAPI.Models;
+
+
+namespace ZenkoAPI.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public AccountRepository(DatabaseContext databaseContext)
+        {
+            _databaseContext = databaseContext;
+        }
+
+        public async Task AddUserToDatabaseAsync(User user)
+        {
+            try
+            {
+                await _databaseContext.AddAsync(user);
+                await _databaseContext.SaveChangesAsync();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
