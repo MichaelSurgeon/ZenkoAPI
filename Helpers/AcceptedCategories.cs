a8fa9e2974891c4645d6648d3098d7feaad8bd79
Michael Surgeon <m.surgeon@craneware.com>
2024-06-24 16:03:05 +0100

    add helpers folder

---
+++ Helpers/AcceptedCategories.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Immutable;
+
+namespace ZenkoAPI.Helpers
+{
+    public static class AcceptedCategories
+    {
+        public static ImmutableList<string> categories = ImmutableList.Create<string>
+        (
+            "general",
+            "eating out",
+            "bills",
+            "entertainment",
+            "transport",
+            "shopping",
+            "groceries",
+            "subscriptions",
+            "debt"
+        );
+    }
+}
